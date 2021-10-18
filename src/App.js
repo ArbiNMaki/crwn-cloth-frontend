aa95eabf4dc17b541b23edce9d1db1d110892766
unknown <arbi.nmaki919@gmail.com>
2021-10-18 22:36:19 +0700

    Update, add on App.js google sign-in 2

--- src/App.js
+++ src/App.js
@@ -6,19 +6,44 @@ import "./App.css";
 import HomePage from "./pages/homepage/Homepage";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
-import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up"
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    auth.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
