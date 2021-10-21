72a9a36e905405e284319a75a7cc2099b567479a
unknown <arbi.nmaki919@gmail.com>
2021-10-21 18:56:03 +0700

    Setting up redux 2

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
